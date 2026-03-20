705f27f47ffce597cff6d9ccd5062742fa04f454
Razi Syed <razisyed03@gmail.com>
2026-03-20 01:34:15 -0400

    Configured github CI

--- wavelength/src/components/RoomClient.tsx
+++ wavelength/src/components/RoomClient.tsx
@@ -1,4 +1,5 @@
 "use client";
+/* eslint-disable react-hooks/set-state-in-effect */
 
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { io } from "socket.io-client";
